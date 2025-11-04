c432abd6d3c2c0c67ba55088deb40e540e0e8508
dvorakchen <dvorakchen@outlook.com>
2025-11-04 14:28:01 +0800

    chore: log

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,12 @@
 import { error, type RequestEvent } from '@sveltejs/kit';
 import type { UserSignInInfo } from '$lib/share/user.ts';
+import logger from "$lib/server/log.ts";
 
 export async function load({ fetch }: RequestEvent) {
 	const response = await fetch(`/api/users/sign-in-info`);
+	logger.info(`+layout.server.ts, after fetch /api/users/sign-in-info`);
 
+	logger.error(`${response.status}`);
 	if (!response.ok) {
 		throw error(response.status, `Failed to fetch sign-in info: ${response.statusText}`);
 	}
