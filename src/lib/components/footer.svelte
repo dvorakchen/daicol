6bad303e92b1a767d5d9afb0c12bcd2dde70c6ed
dvorakchen <dvorakchen@outlook.com>
2025-11-06 23:27:47 +0800

    feat: update background color

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -3,7 +3,7 @@
 	import { env } from '$env/dynamic/public';
 </script>
 
-<footer class="footer-center footer footer-horizontal rounded bg-base-200 p-10 text-base-content">
+<footer class="footer-center footer footer-horizontal rounded bg-transparent p-10 text-base-content">
 	<aside>
 		<p>Copyright © {new Date().getFullYear()} - All right reserved</p>
 		<a href="mailto:birenchens@163.com">{m.contact_us()}: birenchens@163.com</a>
