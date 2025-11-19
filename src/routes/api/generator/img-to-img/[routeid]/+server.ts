3dd180fb778026ed1039a99d66403ff95055061e
dvorakchen <dvorakchen@outlook.com>
2025-11-19 22:30:02 +0800

    feat: use ReferenceImage

--- src/routes/api/generator/img-to-img/[routeid]/+server.ts
+++ src/routes/api/generator/img-to-img/[routeid]/+server.ts
@@ -1,11 +1,18 @@
 import { json, type RequestEvent } from '@sveltejs/kit';
-import { blendPlugInPrompt, UPLOAD_IMAGE_MAX_SIZE } from '$lib/share/index.ts';
+import {
+	blendPlugInPrompt,
+	getFileMIME,
+	getFilename,
+	UPLOAD_IMAGE_MAX_SIZE
+} from '$lib/share/index.ts';
 import {
 	type Generator,
 	generatorServiceId,
 	type ReferenceImage
 } from '$lib/server/generator/index.ts';
 import { type AppRepo, appRepoServiceId } from '$lib/server/repo/apps/index.ts';
+import type { Logger } from '$lib/server/logger/index.ts';
+import { bucketServiceId, type Bucket } from '$lib/server/file-store.ts';
 
 const FILE_KEY_IN_FORMDATA = 'file';
 
@@ -40,13 +47,7 @@ export async function POST({ request, params, locals }: RequestEvent) {
 
 	const referFilesData: ReferenceImage[] = [];
 
-	for (const file of files) {
-		locals.logger.info(`filetype: ${file.type}`);
-		referFilesData.push({
-			mimeType: file.type,
-			content: await file.arrayBuffer()
-		} as ReferenceImage);
-	}
+	referFilesData.push(...(await files2ReferenceImage(files, locals.logger)));
 
 	// const referImgs = (await getReferenceImgs(+routeId)).map(
 	// 	(img) =>
@@ -61,7 +62,17 @@ export async function POST({ request, params, locals }: RequestEvent) {
 	locals.logger.info(`all reference images: ${referFilesData.length}`);
 	const appRepo = locals.di.get<AppRepo>(appRepoServiceId);
 
-	let prompt = await appRepo.getPrompt(+routeId);
+	const app = await appRepo.getAppByRouteId(+routeId, false);
+	if (!app) {
+		return new Response(null, { status: 422 });
+	}
+
+	if ((app.referenceImgs?.length ?? 0) > 0) {
+		const bucket = locals.di.get<Bucket>(bucketServiceId);
+		referFilesData.push(...(await fileUrls2ReferenceImage(app.referenceImgs!, bucket)));
+	}
+
+	let prompt = app.prompt;
 	locals.logger.info(prompt);
 
 	prompt = blendPlugInPrompt(prompt, promptPlugIn);
@@ -74,4 +85,36 @@ export async function POST({ request, params, locals }: RequestEvent) {
 	await appRepo.increaseUsedCount(+routeId);
 
 	return json({ urls });
+}
+
+async function files2ReferenceImage(files: File[], logger: Logger): Promise<ReferenceImage[]> {
+	const list = [];
+	for (const file of files) {
+		logger.info(`filetype: ${file.type}`);
+		list.push({
+			mimeType: file.type,
+			content: await file.arrayBuffer()
+		} as ReferenceImage);
+	}
+
+	return list;
+}
+
+async function fileUrls2ReferenceImage(urls: string[], bucket: Bucket): Promise<ReferenceImage[]> {
+	const list: ReferenceImage[] = [];
+
+	for (const url of urls) {
+		const filename = getFilename(url);
+		const buf = await bucket.getFile(filename);
+		if (!buf) {
+			continue;
+		}
+
+		list.push({
+			mimeType: getFileMIME(filename),
+			content: buf.buffer
+		} as ReferenceImage);
+	}
+
+	return list;
 }
